170b3f20971c3addcdb425b7ccf3c3ef0938ad5d
ds-ds-00 <drolex2004@gmail.com>
2025-06-30 17:23:53 +0000

    conversion

---
+++ basics/conversion_operation.js
@@ -0,0 +1,16 @@
+// let score = "30"
+let score = undefined
+// let score = null
+console.log(typeof(score));
+
+let valueInNo=Number(score)
+console.log(typeof valueInNo);
+console.log( valueInNo);
+
+
+let isloggedIn=1  // empty=false, 1=true, anythingvalue = true
+let booleanIsloggedIn=Boolean(isloggedIn)
+console.log(booleanIsloggedIn)
+
+let stringNo=33
+console.log(typeof(String(stringNo)))
