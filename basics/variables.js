e120d62cf8033c2ecd00f643feedadd8a48dcc4b
ds-ds-00 <drolex2004@gmail.com>
2025-06-30 16:24:51 +0000

    Add and update basics/variables.js

---
+++ basics/variables.js
@@ -0,0 +1,22 @@
+const accId=144553
+let accEmail="suvam@google.com"
+var accPass = "1234"
+accCity="kolkata"
+let accState;
+
+// accId=2  //not allowed for constant
+
+accEmail="suvam@dss.com"
+accPass="23456"//got changed
+accCity="chandigarh"
+
+
+console.log(accId);
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+ */
+
+console.table([accId,accEmail,accPass,accCity,accState])
