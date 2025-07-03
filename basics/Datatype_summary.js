9d08afb683657a31f728e756953a857c482a21a9
ds-ds-00 <drolex2004@gmail.com>
2025-07-03 11:38:42 +0000

    datatype

---
+++ basics/Datatype_summary.js
@@ -0,0 +1,10 @@
+// primitive datatype 
+
+// 7catagories : string ,number, boolean,null,undefined,symbol,bigint
+
+
+// reference type(non primitive)
+
+// Array , objects, Functions
+
+// it is a dynamic language
