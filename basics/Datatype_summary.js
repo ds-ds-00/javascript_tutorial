80be7a1c9c574bcc53ba9a5fd3dea2a30d84b79c
ds-ds-00 <drolex2004@gmail.com>
2025-07-03 11:41:05 +0000

    datatype

--- basics/Datatype_summary.js
+++ basics/Datatype_summary.js
@@ -3,6 +3,12 @@
 // 7catagories : string ,number, boolean,null,undefined,symbol,bigint
 
 
+const id =Symbol('123')
+const anotherid =Symbol('123')
+console.log(id===anotherid);
+
+
+
 // reference type(non primitive)
 
 // Array , objects, Functions
